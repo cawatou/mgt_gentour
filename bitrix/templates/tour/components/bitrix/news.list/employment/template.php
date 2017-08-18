6f3dec0c7f8eb34155d26cb98058bfe9fcf864bd
cawatou <cawatou@gmail.com>
2017-08-18 17:28:12 +0300

    co-worker vote and img fix

--- bitrix/templates/tour/components/bitrix/news.list/employment/template.php
+++ bitrix/templates/tour/components/bitrix/news.list/employment/template.php
@@ -1,7 +1,7 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
-<div class="item tt">
+<div class="item fr">
 	<div class="<?if($arItem["PROPERTIES"]['winner']['VALUE_XML_ID']=="5e8334d1984dd9c9791ad47abb05476f"){?>winnerinner<?}?> area">
 		<div class="imgbox2" style="background-image:url(<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>);background-size:cover;"><img src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"></div>
 		<h3><?echo $arItem["NAME"]?></h3>
