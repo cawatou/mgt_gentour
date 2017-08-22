a2fb1f0fe261e37c2696831c7c9db7f04820dfbe
cawatou <cawatou@gmail.com>
2017-08-22 16:38:23 +0300

    modal what is this franchise?

--- content/franchize/index.php
+++ content/franchize/index.php
@@ -209,52 +209,42 @@ $APPLICATION->SetTitle("Франшиза");?>
 	.calcs {
 		margin-bottom:80px;
 	}
+
+	#what_franchise .modal-body img {
+		width: 100%;
+	}
 </style>
 <? // --------Получаем слайдер--------
-
 $arSelect = Array("ID", "NAME", "PROPERTY_*", "IBLOCK_ID", "PREVIEW_TEXT");
-$arFilter = array(
-	"IBLOCK_ID" => 31
-
-	);
+$arFilter = array("IBLOCK_ID" => 31);
 $sliderObj = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
 
 // --------Получаем преимущества--------
-
 $arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT");
-$arFilter = array(
-	"IBLOCK_ID" => 32
-
-	);
+$arFilter = array("IBLOCK_ID" => 32);
 $advantagesObj = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>4), $arSelect);
 
 // --------Получаем видео для франшизы--------
-
 $arSelect = Array("ID", "NAME", "PROPERTY_*", "IBLOCK_ID");
-$arFilter = array(
-	"IBLOCK_ID" => 33
-	);
+$arFilter = array("IBLOCK_ID" => 33);
 $videoObj = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>3), $arSelect);
 
 // --------Получаем финансовые показатели--------
-
 $arSelect = Array("ID", "NAME", "PROPERTY_*", "IBLOCK_ID");
-$arFilter = array(
-	"IBLOCK_ID" => 34
-
-	);
+$arFilter = array("IBLOCK_ID" => 34);
 $financialObj = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>4), $arSelect);
 
 // --------Получаем контент для блока "Почему мы"--------
-
 $arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT");
-$arFilter = array(
-	"IBLOCK_ID" => 35
-
-	);
+$arFilter = array("IBLOCK_ID" => 35);
 $whyWeObj = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>1), $arSelect);
-?>
 
+// --------Получаем контент для блока "Что такое франшиза?"--------
+$arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT");
+$arFilter = array("IBLOCK_ID" => 39);
+$what_franch_res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>1), $arSelect);
+while($ob = $what_franch_res->GetNextElement())	$what_franch = $ob->GetFields();
+?>
 <div class="container franchblog">
 	<div class="row ">
 		<div class="col-md-12">
@@ -286,188 +276,201 @@ $whyWeObj = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"
 											<i class="fa fa-search"></i>
 										</div>
 									</div>
-									<?}?>
-								</div>
+								<?}?>
 							</div>
 						</div>
-					<?}?>
-				</div>
-			</div>
-			<div class="col-md-3">
-				<div class="franchorder">
-					<h2>Заявка</h2>
-					<form class="franchise">
-						<div class="form-group ">
-							<label for="iname">Имя</label>
-							<input id="iname" class="form-control" name="name"  />
-						</div>
-						<div class="form-group ">
-							<label for="phone">Телефон</label>
-							<input id="phone" class="form-control" name="phone" placeholder="+7(___)___-__-__" />
-						</div>
-						<button type="submit" class=" btnformmodalred">отправить заявку</button>
-					</form>
-				</div>
-			</div>
-		</div>
-		<div class="row ">
-			<div class="col-md-12">
-				<h1>Наши приемущества</h1>
-			</div>
-		</div>
-		<div class="row advant">
-			<?while($ob = $advantagesObj->GetNextElement()){ 
-				$arFields = $ob->GetFields();  
-				$arProps = $ob->GetProperties();?>
-				<div class="col-md-3">
-					<div class="circle"> 
-						&nbsp;<img src="<?=CFile::GetPath($arFields["PREVIEW_PICTURE"])?>"> 
 					</div>
-					<b><?=$arFields['NAME']?></b>
+				<?}?>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<div class="franchorder">
+				<h2>Заявка</h2>
+				<form class="franchise">
+					<div class="form-group ">
+						<label for="iname">Имя</label>
+						<input id="iname" class="form-control" name="name"  />
+					</div>
+					<div class="form-group ">
+						<label for="phone">Телефон</label>
+						<input id="phone" class="form-control" name="phone" placeholder="+7(___)___-__-__" />
+					</div>
+					<button type="submit" class=" btnformmodalred">отправить заявку</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="row ">
+		<div class="col-md-12">
+			<h1>Наши приемущества</h1>
+		</div>
+	</div>
+	<div class="row advant">
+		<?while($ob = $advantagesObj->GetNextElement()){ 
+			$arFields = $ob->GetFields();  
+			$arProps = $ob->GetProperties();?>
+			<div class="col-md-3">
+				<div class="circle"> 
+					&nbsp;<img src="<?=CFile::GetPath($arFields["PREVIEW_PICTURE"])?>"> 
+				</div>
+				<b><?=$arFields['NAME']?></b>
+				<p><?=$arFields['PREVIEW_TEXT']?></p>
+			</div>
+		<?}?>
+	</div>
+	<link rel="stylesheet" href="/video/plyr.css">
+	<div class="row video">
+		<?while($ob = $videoObj->GetNextElement()){ 
+			$arFields = $ob->GetFields();  
+			$arProps = $ob->GetProperties();?>
+			<div class="col-md-4">
+				<h3 class="rew"><?=$arFields['NAME']?></h3>
+				<video controls="controls" width="100%" poster="<?=CFile::GetPath($arProps['PREVIEW_IMAGE_FOR_VIDEO']['VALUE'])?>" crossorigin>
+					<source src="<?=CFile::GetPath($arProps['VIDEOMP4']['VALUE'])?>" type='video/mp4;'>
+					<source src="<?=CFile::GetPath($arProps['VIDEOWEBM']['VALUE'])?>" type='video/webm;'>
+					<source src="<?=CFile::GetPath($arProps['VIDEOOGV']['VALUE'])?>" type='video/ogv;'>
+						Видео не поддерживается вашим браузером.
+				</video>
+			</div>
+		<? }?>
+	</div>
+	<div class="row ">
+		<div class=" col-md-12" >
+			<h1>Калькулятор франшизы</h1>
+		</div>
+	</div>
+	<div class="row calcs">
+		<div class=" col-md-12" >
+			<div id="calculation_franchize" class="box calc">
+				<div class="form-group ">
+					<label for="citizens_amount">Население города</label>
+					<select id="citizens_amount" class="form-control"  >
+						<option value="0">0-10 тыс.</option>
+						<option value="1">11 - 30 тыс</option>
+						<option value="2">31 - 50 тыс</option>
+						<option value="3">51 - 100 тыс</option>
+						<option value="4">101 - 300 тыс</option>
+						<option value="5">301 - 500 тыс</option>
+						<option value="6">501 тыс - 1 млн</option>
+						<option value="7">1-3 млн</option>
+						<option value="8">3 млн и более</option>
+					</select>
+				</div>
+				<!-- <div class="form-group ">
+					<label for="cntcity">Название города</label>
+					<input id="cntcity" class="form-control"  />
+				</div> -->
+				<h4>Объем продаж туров за последние 3 месяца</h4>
+				<div class="form-group ">
+					<label for="fmonth">Первый месяц</label>
+					<input id="fmonth" class="form-control" placeholder="Количество (шт.)" />
+				</div>
+				<div class="form-group ">
+					<label for="nmonth">Второй месяц</label>
+					<input id="nmonth" class="form-control"  placeholder="Количество (шт.)" />
+				</div>
+				<div class="form-group ">
+					<label for="lmonth">Прошлый месяц</label>
+					<input id="lmonth" class="form-control" placeholder="Количество (шт.)"  />
+				</div>
+				<div class="form-group ">
+					<input id="submitchart" type="submit" class="btn btn-danger" value="Рассчитать на графике"  />
+				</div>
+			</div>
+			<div class="box">
+				<h4>Объем ваших продаж под брендом<br> <span>&laquo;Мой Горящий Тур&raquo;</span></h4>
+
+				<canvas id="chart" width="300"></canvas>
+			</div>
+		</div>
+	</div>
+	<div class="row ">
+		<div class=" col-md-12" >
+			<h1>Финансовые показатели</h1>
+		</div>
+	</div>
+	<div class="row finan">	
+	<?while($ob = $financialObj->GetNextElement()){ 
+	$arFields = $ob->GetFields();  
+	$arProps = $ob->GetProperties();?>
+		<div class=" col-md-3" >
+			<div class="buble">
+				<b><?=$arProps['VALUE_IN_CIRCLE']['VALUE']?></b>
+			</div>
+			<span><?=$arFields['NAME']?></span>
+		</div>	
+	<? }?>
+	</div>
+	<div class="row whyme">
+		<div class="col-md-12">
+			<div class="row ">
+			<?while($ob = $whyWeObj->GetNextElement()){ 
+			$arFields = $ob->GetFields();  
+			$arProps = $ob->GetProperties();?>
+				<div class="col-md-7">
+					<h4><?=$arFields['NAME']?></h4>
 					<p><?=$arFields['PREVIEW_TEXT']?></p>
 				</div>
-			<?}?>
+				<div class="col-md-5">
+					<img src="<?=CFile::GetPath($arFields['PREVIEW_PICTURE'])?>" width="100%"/>
+				</div>
+			<? }?>
+			</div>
 		</div>
-		<link rel="stylesheet" href="/video/plyr.css">
-		<div class="row video">
-			<?while($ob = $videoObj->GetNextElement()){ 
-				$arFields = $ob->GetFields();  
-				$arProps = $ob->GetProperties();?>
-				<div class="col-md-4">
-					<h3 class="rew"><?=$arFields['NAME']?></h3>
-					<video controls="controls" width="100%" poster="<?=CFile::GetPath($arProps['PREVIEW_IMAGE_FOR_VIDEO']['VALUE'])?>" crossorigin>
-						<source src="<?=CFile::GetPath($arProps['VIDEOMP4']['VALUE'])?>" type='video/mp4;'>
-						<source src="<?=CFile::GetPath($arProps['VIDEOWEBM']['VALUE'])?>" type='video/webm;'>
-						<source src="<?=CFile::GetPath($arProps['VIDEOOGV']['VALUE'])?>" type='video/ogv;'>
-							Видео не поддерживается вашим браузером.
-						</video>
-					</div>
-				<? }?>
+	</div>
+</div>
+<div class="container-fluid link-room" >
+	<div class="container">
+		<div class="row ">
+			<div class="col-md-12">
+				<a href="skype:franchise_154?call" class="linkmenu">позвонить по Skype</a>
+				<a href="#" class="linkmenu"  data-toggle="modal" data-target="#question_franchise">Задать вопрос</a>
+				<a href="#" class="linkmenu" data-toggle="modal" data-target="#franchform">Заявка на презентацию франшизы онлайн</a>
+				<a href="#" class="what" data-toggle="modal" data-target="#what_franchise">Что такое франшиза?</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="franchform" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<? $APPLICATION->IncludeComponent("bitrix:form.result.new", "question", Array(
+				"SEF_MODE" => "N",
+				"WEB_FORM_ID" => 10,
+				"EDIT_URL" => "",
+				"CHAIN_ITEM_TEXT" => "",
+				"CHAIN_ITEM_LINK" => "",
+				"IGNORE_CUSTOM_TEMPLATE" => "Y",
+				"SUCCESS_URL" => "",
+				"COMPONENT_TEMPLATE" => "question",
+				"LIST_URL" => "result.php",
+				"USE_EXTENDED_ERRORS" => "Y",
+				"CACHE_TYPE" => "A",
+				"CACHE_TIME" => "3600",
+				"VARIABLE_ALIASES" => Array("RESULT_ID" => "RESULT_ID", "WEB_FORM_ID" => "WEB_FORM_ID")
+				)
+			);?>
+		</div>
+	</div>
+</div>
+	<div class="modal fade" id="what_franchise" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel"><?=$what_franch['NAME']?></h4>
 				</div>
-				<div class="row ">
-					<div class=" col-md-12" >
-						<h1>Калькулятор франшизы</h1>
-					</div>
-				</div>
-				<div class="row calcs">
-					<div class=" col-md-12" >
-						<div id="calculation_franchize" class="box calc">
-							<div class="form-group ">
-								<label for="citizens_amount">Население города</label>
-								<select id="citizens_amount" class="form-control"  >
-									<option value="0">0-10 тыс.</option>
-									<option value="1">11 - 30 тыс</option>
-									<option value="2">31 - 50 тыс</option>
-									<option value="3">51 - 100 тыс</option>
-									<option value="4">101 - 300 тыс</option>
-									<option value="5">301 - 500 тыс</option>
-									<option value="6">501 тыс - 1 млн</option>
-									<option value="7">1-3 млн</option>
-									<option value="8">3 млн и более</option>
-								</select>
-							</div>
-							<!-- <div class="form-group ">
-								<label for="cntcity">Название города</label>
-								<input id="cntcity" class="form-control"  />
-							</div> -->
-							<h4>Объем продаж туров за последние 3 месяца</h4>
-							<div class="form-group ">
-								<label for="fmonth">Первый месяц</label>
-								<input id="fmonth" class="form-control" placeholder="Количество (шт.)" />
-							</div>
-							<div class="form-group ">
-								<label for="nmonth">Второй месяц</label>
-								<input id="nmonth" class="form-control"  placeholder="Количество (шт.)" />
-							</div>
-							<div class="form-group ">
-								<label for="lmonth">Прошлый месяц</label>
-								<input id="lmonth" class="form-control" placeholder="Количество (шт.)"  />
-							</div>
-							<div class="form-group ">
-								<input id="submitchart" type="submit" class="btn btn-danger" value="Рассчитать на графике"  />
-							</div>
-						</div>
-						<div class="box">
-							<h4>Объем ваших продаж под брендом<br> <span>&laquo;Мой Горящий Тур&raquo;</span></h4>
-
-							<canvas id="chart" width="300"></canvas>
-						</div>
-					</div>
-				</div>
-				<div class="row ">
-					<div class=" col-md-12" >
-						<h1>Финансовые показатели</h1>
-					</div>
-				</div>
-				<div class="row finan">	
-				<?while($ob = $financialObj->GetNextElement()){ 
-				$arFields = $ob->GetFields();  
-				$arProps = $ob->GetProperties();?>
-					<div class=" col-md-3" >
-						<div class="buble">
-							<b><?=$arProps['VALUE_IN_CIRCLE']['VALUE']?></b>
-						</div>
-						<span><?=$arFields['NAME']?></span>
-					</div>	
-				<? }?>
-				</div>
-				<div class="row whyme">
-					<div class="col-md-12">
-						<div class="row ">
-						<?while($ob = $whyWeObj->GetNextElement()){ 
-						$arFields = $ob->GetFields();  
-						$arProps = $ob->GetProperties();?>
-							<div class="col-md-7">
-								<h4><?=$arFields['NAME']?></h4>
-								<p><?=$arFields['PREVIEW_TEXT']?></p>
-							</div>
-							<div class="col-md-5">
-								<img src="<?=CFile::GetPath($arFields['PREVIEW_PICTURE'])?>" width="100%"/>
-							</div>
-						<? }?>
-						</div>
-					</div>
+				<div class="modal-body">
+					<?=$what_franch['PREVIEW_TEXT']?>
 				</div>
 			</div>
-			<div class="container-fluid link-room" >
-				<div class="container">
-					<div class="row ">
-						<div class="col-md-12">
-							<a href="skype:franchise_154?call" class="linkmenu">позвонить по Skype</a>
-							<a href="#" class="linkmenu"  data-toggle="modal" data-target="#question_franchise">Задать вопрос</a>
-							<a href="#" class="linkmenu" data-toggle="modal" data-target="#franchform">Заявка на презентацию франшизы онлайн</a>
-							<a href="#" class="what">Что такое франшиза?</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="modal fade" id="franchform" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-				<div class="modal-dialog" role="document">
-					<div class="modal-content">
-						<? $APPLICATION->IncludeComponent("bitrix:form.result.new", "question", Array(
-							"SEF_MODE" => "N",
-							"WEB_FORM_ID" => 10,
-							"EDIT_URL" => "",
-							"CHAIN_ITEM_TEXT" => "",
-							"CHAIN_ITEM_LINK" => "",
-							"IGNORE_CUSTOM_TEMPLATE" => "Y",
-							"SUCCESS_URL" => "",
-							"COMPONENT_TEMPLATE" => "question",
-							"LIST_URL" => "result.php",
-							"USE_EXTENDED_ERRORS" => "Y",
-							"CACHE_TYPE" => "A",
-							"CACHE_TIME" => "3600",
-							"VARIABLE_ALIASES" => Array("RESULT_ID" => "RESULT_ID", "WEB_FORM_ID" => "WEB_FORM_ID")
-							)
-						);?>
-						</div>
-					</div>
-				</div>
-				<div class="container-fluid footer-all" >
-					<?$APPLICATION->IncludeFile(
-						$APPLICATION->GetTemplatePath("footer_inc.php"),
-						Array(),
-						Array("MODE"=>"html")
-						);?>
-					</div>
-					<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+		</div>
+	</div>
+<div class="container-fluid footer-all" >
+	<?$APPLICATION->IncludeFile(
+		$APPLICATION->GetTemplatePath("footer_inc.php"),
+		Array(),
+		Array("MODE"=>"html")
+		);?>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
